538f91174a42e2b812bae8c092263c5a9144443d
Your Name <you@example.com>
2024-06-27 02:33:58 +0300

    some ref

--- lib/core/widgets/custom_snack_bar.dart
+++ lib/core/widgets/custom_snack_bar.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:masrofatak/core/app_styles/app_styles.dart';
-import 'package:masrofatak/core/app_theme/colors/app_colors.dart';
 
 abstract class CustomSnackBar {
   static customSnackBar(BuildContext context,
       {required String text, Color? color}) {
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
       duration: const Duration(seconds: 4 ),
       padding: EdgeInsets.symmetric(horizontal: 15.w , vertical: 5.h  ),
-      
+      elevation: 0 ,
+      dismissDirection: DismissDirection.up ,
+       behavior: SnackBarBehavior.floating,
+       margin: EdgeInsets.only(left: 20.w , right: 20.w , bottom: MediaQuery.sizeOf(context).height - 210   ),  
       content: Text(text,
           style:
-              AppStyles.styleMedium12.copyWith(color: AppColors.color424242)),
+              AppStyles.styleRegular14.copyWith(color: Colors.black )),
       backgroundColor: color ?? Colors.green,
-      shape: OutlineInputBorder(borderRadius: BorderRadius.circular(15.r)),
+      shape: OutlineInputBorder(borderRadius: BorderRadius.circular(10.r) ,
+       borderSide: BorderSide.none
+      ),
     ));
   }
 }
