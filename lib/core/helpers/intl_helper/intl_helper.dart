c9b2643af641fd822b7e6ef2102723f9023793ff
Your Name <you@example.com>
2024-06-28 00:31:59 +0300

    add intl helper

---
+++ lib/core/helpers/intl_helper/intl_helper.dart
@@ -0,0 +1,4 @@
+abstract class IntlHelper {
+  static final dayNow = DateTime.now().day.toString();
+  static final monthNow = DateTime.now().month.toString();
+}
--- lib/features/expenses_income/data/models/expenses_income_model.dart
+++ lib/features/expenses_income/data/models/expenses_income_model.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:hive_flutter/adapters.dart';
+import 'package:masrofatak/core/helpers/intl_helper/intl_helper.dart';
 
 import 'package:masrofatak/features/categories/data/models/categories_model.dart';
 
@@ -29,8 +30,8 @@ class ExpensesIncomeModel extends HiveObject {
     this.amount,
     this.description,
   }) {
-    day = DateTime.now().day.toString();
-    month = DateTime.now().month.toString();
+    day = IntlHelper.dayNow;
+    month = IntlHelper.monthNow;
     category =
         CategoryModel(name: 'Grocery', id: 0, icon: Assets.imagesShopping);
   }
